23cdbc3670f14de9b454d7085bc0bc4c6cdfd7f4
Wesley Rocha <wesley.cloudmed.io>
2020-01-14 19:44:08 -0300

    first commit

---
+++ src/layout/AppFooter.vue
@@ -0,0 +1,16 @@
+<template>
+    <footer class="footer has-cards">
+    </footer>
+</template>
+<script>
+export default {
+  name: 'app-footer',
+  data() {
+    return {
+      year: new Date().getFullYear()
+    }
+  }
+};
+</script>
+<style>
+</style>
---
+++ src/layout/AppHeader.vue
@@ -0,0 +1,21 @@
+<template>
+    <header class="header-global">
+        <base-nav class="navbar-main" transparent type="" effect="light" expand>
+        </base-nav>
+    </header>
+</template>
+<script>
+import BaseNav from "@/components/BaseNav";
+import BaseDropdown from "@/components/BaseDropdown";
+import CloseButton from "@/components/CloseButton";
+
+export default {
+  components: {
+    BaseNav,
+    CloseButton,
+    BaseDropdown
+  }
+};
+</script>
+<style>
+</style>
