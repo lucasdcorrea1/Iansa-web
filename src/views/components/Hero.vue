bbbaafe1f99e42b0576fc204bab6c22e1853eef4
wesley <wesley.puply@gmail.com>
2020-01-16 23:24:19 -0300

    Updating packs

--- src/views/components/Hero.vue
+++ src/views/components/Hero.vue
@@ -32,7 +32,7 @@
                         </div>
                     </div>
                     <div class="col-lg-7 text-center pt-lg">
-                        <img src="img/brand/white.png" style="width: 200px;" class="img-fluid">
+                        <img src="img/brand/logo.png" style="width: 200px;" class="img-fluid">
                         <p class="lead text-white mt-4 mb-5">A beautiful Design System for Bootstrap 4. It's Free and Open Source.</p>
                         <div class="btn-wrapper">
                             <base-button tag="a"
