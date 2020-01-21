47e315dc4d557274e5a32145f1e859fd817dd851
Wesley Rocha <wesley.cloudmed.io>
2020-01-21 19:07:11 -0300

    Updating carousel

--- src/layout/starter/StarterHeader.vue
+++ src/layout/starter/StarterHeader.vue
@@ -2,13 +2,13 @@
     <header class="header-global">
         <base-nav class="navbar-main" transparent type="" effect="light" expand>
             <router-link slot="brand" class="navbar-brand mr-lg-5" to="/">
-                <img src="img/brand/logo.png">
+                <img src="img/brand/iansa-logo.png">
             </router-link>
 
             <div class="row" slot="content-header" slot-scope="{closeMenu}">
                 <div class="col-6 collapse-brand">
                     <a href="../index.html">
-                        <img src="img/brand/blue.png">
+                        <img src="img/brand/iansa-logo.png">
                     </a>
                 </div>
                 <div class="col-6 collapse-close">
--- src/views/components/Carousel.vue
+++ src/views/components/Carousel.vue
@@ -1,59 +1,15 @@
 <template>
   <div class="col-lg-12" style="padding: 0">
     <div class="overflow-hidden">
-      <!-- <pre>
-        {{slides}}
-      </pre> -->
-      <!-- <div v-for="(slide, index) in slides" :key="index">
-        {{slide.title}}
-      </div> -->
       <b-carousel class="carousel" id="carousel1" controls indicators>
         <b-carousel-slide 
           v-for="(slide, index) in slides" :key="index"
           :img-src="slide.url">
           {{slide.title}}
         </b-carousel-slide>
-        <!-- Text slides with image -->
-        <!-- <b-carousel-slide img-src="img/theme/img-1-1200x1000.jpg">Legenda</b-carousel-slide>
-        <b-carousel-slide img-src="img/theme/img-2-1200x1000.jpg">Legenda</b-carousel-slide> -->
       </b-carousel>
     </div>
   </div>
-  <!-- <section class="section section-lg section-shaped overflow-hidden my-0">
-        <div class="shape shape-style-1 shape-default shape-skew">
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span>
-        </div>
-        <div class="container py-0 pb-lg">
-            <div class="row justify-content-between align-items-center">
-                <div class="col-lg-5 mb-5 mb-lg-0">
-                    <h1 class="text-white font-weight-light">Bem vindo!</h1>
-                    <p class="lead text-white mt-4">Fique por dentro de todas as novidades do IANSA</p>
-                    <a href="https://demos.creative-tim.com/argon-design-system/docs/components/alerts.html"
-                       class="btn btn-white mt-4">Eventos</a>
-                </div>
-                <div class="col-lg-12 mb-lg-auto">
-                    <div class="rounded shadow-lg overflow-hidden">
-                        <b-carousel id="carousel1"
-                                    controls
-                                    indicators>
-                            Text slides with image
-                            <b-carousel-slide img-src="img/theme/img-1-1200x1000.jpg">
-                                Legenda
-                            </b-carousel-slide>
-                            <b-carousel-slide img-src="img/theme/img-2-1200x1000.jpg">
-                                Legenda
-                            </b-carousel-slide>
-                        </b-carousel>
-                    </div>
-                </div>
-            </div>
-        </div>
-  </section>-->
 </template>
 <script>
 import axios from 'axios';
@@ -107,14 +63,29 @@ div#carousel1___BV_inner_::before {
   left: 0;
   width: 100%;
   height: 100%;
-  bottom: -10px;
+  // bottom: -10px;
   z-index: 1;
   background-image: url('/img/brand/carousel-before.png');
   background-position: 0 bottom;
   background-size: contain;
   background-repeat: no-repeat;
 }
+
 .carousel {
+  .carousel-indicators {
+    li {
+      background-color: #ec95e1;
+      border: 1px solid #f5f5f5;
+      width: 12px;
+      height: 12px;
+      border-radius: 50%;
+      outline: none;
+      &.active {
+        box-shadow: 0 1px 3px rgba(0,0,0,0.24), 0 1px 2px rgba(0,0,0,0.36);
+        background-color: #ab4a9e;
+      }
+    }
+  }
   .carousel-item {
     max-height: 600px;
     // min-height: 600px; 
